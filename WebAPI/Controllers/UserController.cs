7234d3a838b0227ed0288efac88d6eaf6b765602
DavidEggenberger <david99.eggenberger@gmail.com>
2021-12-04 21:06:29 +0100

    OnlineHub

--- WebAPI/Controllers/UserController.cs
+++ WebAPI/Controllers/UserController.cs
@@ -1,11 +1,18 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using DTOs.User;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.SignalR;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Claims;
 using System.Threading.Tasks;
+using WebAPI.Data;
 using WebAPI.Data.Entities;
+using WebAPI.Hubs;
 
 namespace WebAPI.Controllers
 {
@@ -15,10 +22,14 @@ namespace WebAPI.Controllers
     {
         private SignInManager<ApplicationUser> signInManager;
         private UserManager<ApplicationUser> userManager;
-        public UserController(SignInManager<ApplicationUser> signInManager, UserManager<ApplicationUser> userManager)
+        private IHubContext<OnlineHub> onlineHubContext;
+        private ApplicationDbContext applicationDbContext;
+        public UserController(SignInManager<ApplicationUser> signInManager, UserManager<ApplicationUser> userManager, IHubContext<OnlineHub> onlineHubContext, ApplicationDbContext applicationDbContext)
         {
             this.signInManager = signInManager;
             this.userManager = userManager;
+            this.onlineHubContext = onlineHubContext;
+            this.applicationDbContext = applicationDbContext;
         }
         [HttpGet("Login")]
         public async Task<ActionResult> LoginRedirectToGitHub()
@@ -27,6 +38,7 @@ namespace WebAPI.Controllers
             var properties = signInManager.ConfigureExternalAuthenticationProperties("Microsoft", redirectUrl);
             return Challenge(properties, "GitHub");
         }
+        [HttpGet]
         public async Task<IActionResult> ExternalLoginCallback(string ReturnUrl = null)
         {
             var info = await signInManager.GetExternalLoginInfoAsync();
@@ -53,14 +65,36 @@ namespace WebAPI.Controllers
             if (user.PictureURI != pictureURI)
             {
                 user.PictureURI = pictureURI;
+                await userManager.UpdateAsync(user);
             }
-            await userManager.UpdateAsync(user);
             var signInResult = await signInManager.ExternalLoginSignInAsync(info.LoginProvider, info.ProviderKey, isPersistent: false, bypassTwoFactor: false);
             return signInResult switch
             {
                 Microsoft.AspNetCore.Identity.SignInResult { Succeeded: true } => LocalRedirect("/"),
                 _ => Redirect("/Error")
             };
+        }
+        [Authorize]
+        [HttpGet("Logout")]
+        public async Task<ActionResult<BFFUserInfoDTO>> LogoutCurrentUser()
+        {
+            ApplicationUser appUser = await userManager.FindByIdAsync(User.FindFirst(ClaimTypes.NameIdentifier)?.Value);
+            appUser.TabsOpen = 0;
+            appUser.IsOnline = false;
+            await userManager.UpdateAsync(appUser);
+            await onlineHubContext.Clients.All.SendAsync("UpdateOnlineUsers");
+            return SignOut(new AuthenticationProperties
+            {
+                RedirectUri = "/"
+            }, IdentityConstants.ApplicationScheme, IdentityConstants.ExternalScheme);
+        }
+        [HttpGet("onlineUsers")]
+        public ActionResult<List<UserDTO>> GetOnlineUsers()
+        {
+            return applicationDbContext.Users.Select(user => new UserDTO
+            {
+                
+            }).ToList();
         }
     }
 }
--- WebAPI/Hubs/OnlineHub.cs
+++ WebAPI/Hubs/OnlineHub.cs
@@ -1,12 +1,63 @@
-﻿using Microsoft.AspNetCore.SignalR;
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.SignalR;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Claims;
 using System.Threading.Tasks;
+using WebAPI.Data;
+using WebAPI.Data.Entities;
 
 namespace WebAPI.Hubs
 {
     public class OnlineHub : Hub
     {
+        private UserManager<ApplicationUser> userManager;
+        private ApplicationDbContext applicationDbContext;
+        public OnlineHub(UserManager<ApplicationUser> userManager, ApplicationDbContext applicationDbContext)
+        {
+            this.userManager = userManager;
+            this.applicationDbContext = applicationDbContext;
+        }
+        public override async Task OnConnectedAsync()
+        {
+            if (!Context.User.Identity.IsAuthenticated)
+            {
+                return;
+            }
+            ApplicationUser appUser = await userManager.FindByIdAsync(Context.User.FindFirst(ClaimTypes.NameIdentifier)?.Value);
+            if (appUser.IsOnline is false)
+            {
+                appUser.IsOnline = true;
+                appUser.TabsOpen = 1;
+                await userManager.UpdateAsync(appUser);
+                await Clients.All.SendAsync("UpdateOnlineUsers");
+                return;
+            }
+            if (appUser.IsOnline)
+            {
+                appUser.TabsOpen++;
+                await userManager.UpdateAsync(appUser);
+            }
+        }
+        public override async Task OnDisconnectedAsync(Exception ex)
+        {
+            if (!Context.User.Identity.IsAuthenticated)
+            {
+                return;
+            }
+            ApplicationUser appUser = await userManager.FindByIdAsync(Context.User.FindFirst(ClaimTypes.NameIdentifier)?.Value);
+            if (appUser.TabsOpen > 0)
+            {
+                appUser.TabsOpen--;
+                await userManager.UpdateAsync(appUser);
+            }
+            if (appUser.TabsOpen == 0)
+            {
+                appUser.IsOnline = false;
+                await userManager.UpdateAsync(appUser);
+                await Clients.AllExcept(appUser.Id).SendAsync("UpdateOnlineUsers");
+            }
+        }
     }
 }
